9b1305fb40e7f3ccc39ee4e6f2054fa0154bb3ea
r1phord <vlad.gorenkov.1996@mail.ru>
2020-06-05 10:20:47 +0200

    created views for tours app

--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class MainView(View):
+    def get(self, request):
+        return render(
+            request, 'tours/index.html'
+        )
+
+
+class DepartureView(View):
+    def get(self, request, departure='anywhere'):
+        return render(
+            request, 'tours/departure.html', context={
+                'departure': departure,
+            }
+        )
+
+
+class TourView(View):
+    def get(self, request, id=0):
+        return render(
+            request, 'tours/tour.html', context={
+                'id': id,
+            }
+        )
